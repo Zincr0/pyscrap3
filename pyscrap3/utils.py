0daaa1df941f2336b255d81f39e59b55780430e6
Jorge Gálvez <daniel@analitic.cl>
2014-05-12 20:10:41 +0000

    utils mensajería por rabbitmq

---
+++ pyscrap3/utils.py
@@ -0,0 +1,32 @@
+from kombu import Connection
+from urllib.parse import quote_plus
+#from IPython import embed
+
+
+class rabbitMsg():
+    """Envía mensajes a traves de rabbitmq usando 'kombu'
+    https://pypi.python.org/pypi/kombu"""
+
+    def __init__(self, queue, user="guest", passw="guest", host="localhost"):
+        self.user = user
+        self.passw = quote_plus(passw)
+        self.host = host
+        self.urlRabbit = "amqp://" + self.user + ":" + self.passw + "@" + host + "/"
+        self.queueName = queue
+        self.open()
+
+    def open(self):
+        self.conn = Connection(self.urlRabbit)
+        self.cola = self.conn.SimpleQueue(self.queueName)
+
+    def send(self, message):
+        self.cola.put(message)
+
+    def get(self):
+        message = self.cola.get(block=True)
+        result = message.payload
+        message.ack()
+        return result
+
+    def close(self):
+        self.cola.close()
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(fname):
 setup(
     scripts=["bin/wscrap3", "bin/wscrap3.3"],
     name="pyscrap3",
-    version="0.0.11",
+    version="0.0.12",
     author="Daniel Mondaca",
     author_email="daniel@analitic.cl",
     description=("micro framework for web scraping"),
